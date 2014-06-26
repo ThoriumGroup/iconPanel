7b1192518e450121e0a8030faa66875ecf37d930
Sean Wallitsch <shidarin@alphamatte.com>
2014-06-25 20:56:24 -0700

    Finished refactor. Icons will now be loaded dynamically based on contents of EXE_PATH/plugins/icons/

--- iconPanel/iconPanel.py
+++ iconPanel/iconPanel.py
@@ -45,7 +45,9 @@ SOFTWARE.
 # =============================================================================
 
 # Standard Imports
+import fnmatch
 import os
+import re
 
 # Nuke Imports
 try:
@@ -63,6 +65,46 @@ __all__ = [
 ]
 
 # =============================================================================
+# PRIVATE FUNCTIONS
+# =============================================================================
+
+
+def _find_files(directory, patterns):
+    """Walks a directory recursively finding all files matching unix pattern.
+
+    Args:
+        directory : (str)
+            The directory to search in.
+
+        patterns : [str]
+            A list of unix style patterns to search for. These should be the
+            same patterns that fnmatch or glob would take, and not regex.
+
+    Returns:
+        [str]
+            A list of filenames matching one of the patterns is returned. The
+            filenames are relative to the directory we were given.
+
+    Raises:
+        N/A
+
+    """
+    matches = []
+    pat = re.compile('|'.join(fnmatch.translate(grep) for grep in patterns))
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if pat.match(basename):
+                filename = os.path.relpath(
+                    os.path.join(root, basename),
+                    directory
+                )
+                matches.append(filename)
+
+    matches.sort()
+
+    return matches
+
+# =============================================================================
 # CLASSES
 # =============================================================================
 
@@ -77,17 +119,29 @@ class IconPanel(nukescripts.PythonPanel):
             'com.thorium.IconPanel'
         )
 
-        self.icons = ('NukeApp.png', 'frame.png', 'SliderThumb.png', 'Eyedropper.png', 'ArrowWhiteLeft.png', 'ArrowWhiteRight.png', 'arrow_left.png', 'arrow_right.png', 'arrow_up.png', 'arrow_down.png', 'next.png', 'previous.png', 'undo.png', 'redo.png', 'revert.png', 'Add.png', 'Remove.png', 'TCStart.png', 'TCEnd.png', 'TCFrameBackward.png', 'TCFrameForward.png', 'TCKeyBackward.png', 'TCKeyForward.png', 'TCPlayBackward.png', 'TCPlayForward.png', 'TCStop.png', 'TCIntervalBackward.png', 'TCIntervalForward.png', 'RangeBackward.png', 'RangeForward.png', 'GroupShow.png', 'Disable.png', 'Cached.png', 'HideInput.png', 'PostageStamp.png', 'Help.png', 'Bold.png', 'Italic.png', 'File_Knob.png', 'Color_Knob.png', 'MultiView.png', 'SingleView.png', 'Curve_Button.png', 'Color_Knob.png', 'SVG/VisibleCurrentFrame.svg', 'SVG/MoveUpOne.svg', 'SVG/Reveal.svg', 'ShowStructure.png', 'select.png', 'AxisIcon.png', 'FolderIcon.png', 'GeoSelect_Nodes_16x16.png', 'GeoSelect_Vertices_16x16.png', 'GeoSelect_OcclusionTest_16x16.png', 'show_points.png', 'show_point_num.png', 'show_point_info.png', 'show_point_normals.png', 'show_point_uvs.png', 'show_vertex_num.png', 'show_vertex_normals.png', 'show_vertex_uvs.png', 'show_prim_num.png', 'show_prim_normals.png', 'show_prim_bbox.png', 'grid_display.png', 'View.png', 'Lock.png', 'Unlock.png', 'Camera.png', 'Refresh.png', 'ROI.png', 'Proxy.png', 'Pause.png', 'IP.png', 'zebra.png', 'FrameRangeLock.png', 'FrameRangeUnlock.png', 'MonitorOut.png', 'Toggle3DToolbar_16x16.png', 'Loop.png', 'Bounce.png', 'StopPlay.png', 'Plugin.png', 'ToolbarImage.png', 'ToolbarDraw.png', 'ToolbarTime.png', 'ToolbarChannel.png', 'ToolbarColor.png', 'Toolbar3DLUT.png', 'ToolbarFilter.png', 'ToolbarKeyer.png', 'ToolbarMerge.png', 'ToolbarTransform.png', 'Toolbar3D.png', 'Toolbar3DLights.png', 'ToolbarViews.png', 'ToolbarStereo.png', 'ToolbarOther.png', 'ToolbarOFX.png', 'AllPlugins.png', 'SVG/Eraser.svg', 'SVG/Selection.svg', 'SVG/MoveToTop.svg', 'SVG/Paint.svg', 'CenterThisNode12p.png', 'QuestionMark12p.png', 'SVG/VisibleSpecificRange.svg', 'SVG/Clone.svg', 'SVG/MoveDownOne.svg', 'SVG/VisibleFromNowOn.svg', 'SVG/Key.svg', 'SVG/UnlimitedLife.svg', 'SVG/MoveToBottom.svg', 'SVG/VisibleUpTillNow.svg', 'ScriptEditor/clearHistory.png', 'ScriptEditor/source.png', 'ScriptEditor/load.png', 'ScriptEditor/save.png', 'ScriptEditor/run.png', 'ScriptEditor/inputOn.png', 'ScriptEditor/inputOff.png', 'ScriptEditor/outputOn.png', 'ScriptEditor/outputOff.png', 'ScriptEditor/bothOn.png', 'ScriptEditor/bothOff.png', 'ScriptEditor/clearOutput.png', 'ControlPanelBin/clear.png', 'ControlPanelBin/lock.png', 'ControlPanelBin/unlock.png', 'CursorTranslate.png', 'CursorRotateNW.png', 'CursorRotateNE.png', 'CursorRotateSW.png', 'CursorRotateSE.png', 'CursorSizeAll.png', 'CursorSkew.png', 'CursorMovePoint.png', 'CursorAddPoint.png', 'CursorRemovePoint.png', 'CursorClosePath.png', 'CursorFeatherPoint.png', 'CursorRemoveFeatherPoint.png', 'CursorSmoothPoint.png', 'CursorCuspPoint.png', 'KeyLeft.png', 'KeyRight.png', 'KeyPlus.png', 'KeyMinus.png', 'Brush.png', 'Roto/Curve.png', 'Roto/Shape.png', 'Roto/Layer.png', 'Roto/BlendUnion.png', 'Roto/BlendIntersect.png', 'Roto/BlendOver.png', 'Roto/BlendDarken.png', 'Roto/BlendMultiply.png', 'Roto/BlendColorBurn.png', 'Roto/BlendLighten.png', 'Roto/BlendScreen.png', 'Roto/BlendColorDodge.png', 'Roto/BlendAdd.png', 'Roto/BlendOverlay.png', 'Roto/BlendSoftLight.png', 'Roto/BlendHardLight.png', 'Roto/BlendDifference.png', 'Roto/BlendExclusion.png', 'Roto/BlendFrom.png', 'Roto/BlendMinus.png', 'Roto/InvertOn.png', 'Roto/InvertOff.png', 'Roto/MotionBlurOn.png', 'Roto/MotionBlurOff.png', 'Roto/Visible.png', 'Roto/Invisible.png', 'Roto/Color.png', 'Roto/OverlayColor.png', 'Roto/OverlayColor.png', 'Roto/SelectAllTool.png', 'Roto/SelectCurvesTool.png', 'Roto/SelectPointsTool.png', 'Roto/SelectFeatherPointsTool.png', 'Roto/BezierTool.png', 'Roto/BSplineTool.png', 'Roto/EllipseTool.png', 'Roto/RectangleTool.png', 'Roto/AddPointsTool.png', 'Roto/RemovePointsTool.png', 'Roto/CuspPointsTool.png', 'Roto/CurvePointsTool.png', 'Roto/RemoveFeatherTool.png', 'Roto/CloseCurveTool.png', 'Roto/BrushTool.png', 'Roto/PencilTool.png', 'Roto/EraserTool.png', 'Roto/CloneTool.png', 'Roto/RevealTool.png', 'Roto/BlurTool.png', 'Roto/SharpenTool.png', 'Roto/SmearTool.png', 'Roto/DodgeTool.png', 'Roto/BurnTool.png', 'Roto/LinkedFeather.png', 'Roto/UnlinkedFeather.png', 'Roto/SingleKeyframe.png', 'Roto/RippleKeyframe.png', 'Roto/ShowCurvesOnDrag.png', 'Roto/ShowPointsOnDrag.png', 'Roto/ShowAllOnDrag.png', 'Roto/CurvePriority.png', 'Roto/PointPriority.png', 'Roto/ShowTransformNone.png', 'Roto/ShowTransformJack.png', 'Roto/ShowTransformBBox.png', 'Roto/ShowTransformBoth.png', 'Roto/ShowPointNumbers.png', 'Roto/UnlimitedLife.png', 'Roto/VisibleFromNowOn.png', 'Roto/VisibleCurrentFrame.png', 'Roto/VisibleUpTillNow.png', 'Roto/VisibleSpecificRange.png', 'Roto/CloneToolbar.png')
+        self.icons = self.find_icons()
         self.batch = 30
 
         self.build_icon_list()
 
     # =========================================================================
 
-
     @staticmethod
     def build_icon_knob(icon):
-        """Builds an individual icon knob"""
+        """Builds an individual icon knob
+
+        Args:
+            icon : (str)
+                The icon filename relative to the icons directory.
+
+        Returns:
+            (<nuke.String_Knob>)
+                A string knob with the filename and then the icon
+
+        Raises:
+            N/A
+
+        """
         name = os.path.splitext(icon.split('/')[-1])[0]
         icon_string = '{name} <img src=":qrc/images/{icon}">'.format(
             name=name, icon=icon
@@ -110,3 +164,14 @@ class IconPanel(nukescripts.PythonPanel):
 
             icon_knob = self.build_icon_knob(icon)
             self.addKnob(icon_knob)
+
+    # =========================================================================
+
+    @staticmethod
+    def find_icons():
+        """Finds all the icons in the Nuke icon folder"""
+        nuke_dir = os.path.split(nuke.EXE_PATH)[0]
+        icon_path = os.path.join(nuke_dir, 'plugins/icons')
+        icons = _find_files(icon_path, ['*.png', '*.svg'])
+
+        return icons
